7a4f25dd6bd17ad322cb425851439405c0ebb3e4
jamietech <jamie@jamiete.ch>
2017-10-12 18:12:24 +1000

    Reflect pin deletes in storage

--- src/ch/jamiete/hilda/pinboard/PinboardListener.java
+++ src/ch/jamiete/hilda/pinboard/PinboardListener.java
@@ -4,7 +4,11 @@ import ch.jamiete.hilda.Hilda;
 import ch.jamiete.hilda.configuration.Configuration;
 import ch.jamiete.hilda.events.EventHandler;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageDeleteEvent;
 import net.dv8tion.jda.core.events.message.guild.react.GenericGuildMessageReactionEvent;
+import com.google.gson.JsonElement;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 public class PinboardListener {
     private final PinboardPlugin plugin;
@@ -16,7 +20,6 @@ public class PinboardListener {
     @EventHandler
     public void onReaction(final GenericGuildMessageReactionEvent event) {
         final Configuration cfg = this.plugin.getHilda().getConfigurationManager().getConfiguration(this.plugin, event.getGuild().getId());
-
         final TextChannel send = event.getGuild().getTextChannelById(cfg.getString("pinboard", "12345"));
 
         if (send == null) {
@@ -31,6 +34,33 @@ public class PinboardListener {
 
         Hilda.getLogger().fine("Handing over a reaction");
         this.plugin.executor.execute(new PinboardTask(event, cfg));
+    }
+
+    @EventHandler
+    public void onMessageDelete(final GuildMessageDeleteEvent event) {
+        final Configuration cfg = this.plugin.getHilda().getConfigurationManager().getConfiguration(this.plugin, event.getGuild().getId());
+        final TextChannel send = event.getGuild().getTextChannelById(cfg.getString("pinboard", "12345"));
+
+        if (send == null) {
+            return;
+        }
+
+        Iterator<Entry<String, JsonElement>> iterator = cfg.get().entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Entry<String, JsonElement> entry = iterator.next();
+            String value = null;
+
+            try {
+                value = entry.getValue().getAsString();
+            } catch (Exception e) {
+                continue;
+            }
+
+            if (value != null && value.equalsIgnoreCase(event.getMessageId())) {
+                iterator.remove();
+            }
+        }
     }
 
 }
